ba9c44726fa756c64489c7a4efaf49989cc5f5fe
camha91 <crystalhanguyen@gmail.com>
2021-04-05 15:35:20 -0700

    Create ProductItem

--- src/component/ProductItem.js
+++ src/component/ProductItem.js
@@ -2,7 +2,30 @@ import React, { Component } from "react";
 
 class ProductItem extends Component {
   render() {
-    return <div></div>;
+    let { dataPhoneItem } = this.props;
+
+    return (
+      <div className="card text-left text-center">
+        <img
+          style={{ width: "200px", height: "250px", marginLeft: "90px" }}
+          className="card-img-top"
+          src={dataPhoneItem.image}
+          alt={dataPhoneItem.image}
+        />
+        <div className="card-body">
+          <h4 className="card-title">{dataPhoneItem.series}</h4>
+          <p className="card-text">{dataPhoneItem.price.toLocaleString()}</p>
+          <button
+            onClick={() => {
+              this.props.onClickViewDetails(dataPhoneItem);
+            }}
+            className="btn btn-success"
+          >
+            View Details
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
